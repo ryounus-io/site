898cf247b27f4161056ed72fd4fc0d3443c706f8
Rahman Younus <rahman@industrious.agency>
2019-05-31 18:16:50 +0100

    restructure blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+const postsPrefix = 'blog';
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -55,12 +57,15 @@ exports.createPages = ({ graphql, actions }) => {
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
+
+
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
+
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: `/${postsPrefix}${value}`,
     })
   }
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
+import Bio from "../components/blog/bio"
+import Layout from "../components/blog/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
 
