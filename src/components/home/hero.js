7f798d71498adaed607d13c17976b3d35b6cbf61
Rahman Younus <rahman@uttily.com>
2019-07-26 18:20:16 +0100

    wip - added svgs for hero

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import { Link } from "gatsby"
 import Divider from './partial/divider';
-import Terminal from './partial/terminal';
+import Astronaut from "./partial/astronaut"
+import Icon from './partial/icon'
 
 function Hero() {
   return (
     <section className="flex-grow">
-      <div className="text-2xl text-gray-800 font-hairline p-48 mb-32">
-        <h2 className="heading text-nord1 text-6xl">Hello, I'm Rahman.</h2>
-        <h3 className="heading text-nord3 text-2xl">Full-Stack Software Developer</h3>
-        <div>
-          <span className="block my-5">
-            I am a passionate developer with numerous years of experience, I love creating rich bespoke digital experiences on the internet
-          </span>
-          <span className="block my-5">
-            I build open-source software in my free time and occasionaly blog on my experiences in tech
-          </span>
+      <div className="flex">
+        <div className="w-1/2">
+          <div className="text-2xl text-gray-800 font-hairline p-48 mb-32">
+            <h2 className="heading text-nord1 text-6xl">Hello, I'm Rahman.</h2>
+            <h3 className="heading text-nord3 text-2xl">
+              Full-Stack Software Developer
+            </h3>
+            <div>
+              <span className="block my-5">
+                I am a passionate developer with numerous years of experience, I
+                love creating rich bespoke digital experiences on the internet
+              </span>
+              <span className="block my-5">
+                I build open-source software in my free time and occasionaly
+                blog on my experiences in tech
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-1/2">
+          <Icon />
         </div>
       </div>
-
-      {/* <Terminal /> */}
 
       <Divider />
     </section>
